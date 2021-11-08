bd285907f0a4bc37900efe44e299f97e9e14d5e1
Nikita <demenkonikita@gmail.com>
2021-11-08 17:04:16 +0200

    add project

---
+++ src/components/info.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Info = () => (
+   <div>
+      <h2 className="infoTitle">Weather</h2>
+      <p className="infoText">Find out the weather in your city</p>
+   </div>
+)
+
+export default Info;
